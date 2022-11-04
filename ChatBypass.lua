7bd741b5d37de43c4d3063d5d63900cc8b94b91d
Tem <92699212+tEMMIE6823@users.noreply.github.com>
2022-11-04 22:45:03 +1100

    Create ChatBypass.lua

---
+++ ChatBypass.lua
@@ -0,0 +1,43 @@
+-- Easy unpatch with fn XD
+-- Execute and say bad words to your hearts content! You can also add more if you know a bit of lua and how this works lol
+-- Made by AnthonyIsntHere and Not Scary!
+-- Temmie6823 added hn instead of fn since that got patched lol 
+-- Anyone who says otherwise is stupid :p
+
+local Bypasses = loadstring(game:HttpGet("https://raw.githubusercontent.com/AnthonyIsntHere/anthonysrepository/main/misc/Bypasses.lua"))()
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local Remote = ReplicatedStorage:FindFirstChild("SayMessageRequest", true)
+
+local ChatBypass; ChatBypass = hookmetamethod(Remote, "__namecall", function(self, ...)
+    local Method = getnamecallmethod()
+    local Arguments = {...}
+    
+    if self == Remote and Method == "FireServer" then
+        local Message = Arguments[1]
+        local Split = Message:split(" ")
+        local FinalMessage = ""
+
+        for _, x in next, Split do
+            for _, Bypass in next, Bypasses do
+                if x:lower() == _ then
+                    if x:upper() ~= x then
+                        Message = Message:gsub(x, Bypass)
+                        FinalMessage = Message .. " hn"
+                    else
+                        Message = Message:gsub(x, Bypass):upper()
+                        FinalMessage = Message:gsub(x, Bypass):upper() .. " hn"
+                    end
+                end
+            end
+        end
+        
+        if FinalMessage ~= "" then
+            Arguments[1] = FinalMessage
+        end
+        
+        return ChatBypass(self, unpack(Arguments))
+    end
+    
+    return ChatBypass(self, ...)
+end)
